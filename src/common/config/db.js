0c7d6190ba0b687246e08d06146899598ab15090
fzninja <497915773@qq.com>
2016-08-30 17:01:02 +0800

    删除多余文件
    delete

--- src/common/config/db.js
+++ src/common/config/db.js
@@ -20,22 +20,3 @@ export default {
     }
   }
 };
-
-
-// export default {
-//   type: 'mysql',
-//   adapter: {
-//     mysql: {
-//       host: 'localhost',
-//       port: '3306',
-//       database: 'blog',
-//       user: 'root',
-//       password: 'Zz61629631z',
-//       prefix: '',
-//       encoding: 'utf8'
-//     },
-//     mongo: {
-
-//     }
-//   }
-// };
